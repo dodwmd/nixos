017ab8ba9ce5a7bebeb7f19e3c8f94627a2c10ea
Braian A. Diez <bdiez19@gmail.com>
2025-12-28 03:35:00 -0300

    fix(pkgs): chromium & edge issues

--- home/packages/browsers/chromium.nix
+++ home/packages/browsers/chromium.nix
@@ -4,18 +4,15 @@
   ...
 }: let
   chromiumFlags = import ./_chromium-flags.nix;
-
-  chromiumWrapped =
-    pkgs.runCommand "ungoogled-chromium-wrapped" {
-      nativeBuildInputs = [pkgs.makeWrapper];
-    } ''
-      mkdir -p $out/bin
-      chromiumBin=$(find ${pkgs.ungoogled-chromium} -type f -executable -name "*chromium*" | head -n 1)
-      cp "$chromiumBin" $out/bin/ungoogled-chromium
-      chmod +x $out/bin/ungoogled-chromium
-      wrapProgram $out/bin/ungoogled-chromium \
-        ${lib.concatMapStringsSep " \\\n        " (flag: "--add-flags '${flag}'") chromiumFlags.flags}
+  chromiumWrapped = pkgs.symlinkJoin {
+    name = "ungoogled-chromium-wrapped";
+    paths = [pkgs.ungoogled-chromium];
+    buildInputs = [pkgs.makeWrapper];
+    postBuild = ''
+      wrapProgram $out/bin/chromium \
+        ${lib.concatMapStringsSep " \\\n        " (flag: "--add-flags \"${flag}\"") chromiumFlags.flags}
     '';
+  };
 in {
   users.users.linuxmobile.packages = [chromiumWrapped];
   environment.sessionVariables = chromiumFlags.sessionVariables;
--- home/packages/browsers/edge.nix
+++ home/packages/browsers/edge.nix
@@ -1,13 +1,10 @@
 {pkgs, ...}: let
-  edgeWrapped =
-    pkgs.runCommand "microsoft-edge-wrapped" {
-      nativeBuildInputs = [pkgs.makeWrapper];
-    } ''
-      mkdir -p $out/bin
-      edgeBin=$(find ${pkgs.microsoft-edge} -type f -executable -name "*edge*" | head -n 1)
-      cp "$edgeBin" $out/bin/microsoft-edge-stable
-      chmod +x $out/bin/microsoft-edge-stable
-      wrapProgram $out/bin/microsoft-edge-stable \
+  edgeWrapped = pkgs.symlinkJoin {
+    name = "microsoft-edge-wrapped";
+    paths = [pkgs.microsoft-edge];
+    buildInputs = [pkgs.makeWrapper];
+    postBuild = ''
+      wrapProgram $out/bin/microsoft-edge \
         --add-flags "--ignore-gpu-blocklist" \
         --add-flags "--enable-zero-copy" \
         --add-flags "--ozone-platform-hint=auto" \
@@ -16,8 +13,7 @@
         --add-flags "--process-per-site" \
         --add-flags "--enable-features=WebUIDarkMode,UseOzonePlatform,VaapiVideoDecodeLinuxGL,VaapiVideoDecoder,WebRTCPipeWireCapturer,WaylandWindowDecorations"
     '';
+  };
 in {
-  users.users.linuxmobile.packages = [
-    edgeWrapped
-  ];
+  users.users.linuxmobile.packages = [edgeWrapped];
 }
