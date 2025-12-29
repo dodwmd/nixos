7dd73fb80697987ca343c35beaacc3a3a2b62a17
Michael Dodwell <1372930+dodwmd@users.noreply.github.com>
2025-12-29 20:54:45 +1000

    Fix SSH key automation by consolidating user definitions

--- hosts/k3s-worker-02/default.nix
+++ hosts/k3s-worker-02/default.nix
@@ -63,14 +63,6 @@
     ];
   };
 
-  users.users.dodwmd = {
-    isNormalUser = true;
-    createHome = true;
-    extraGroups = [ "wheel" "docker" ];
-    openssh.authorizedKeys.keys = [
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAII1Vk18qExSQM6rksG500xD/mgACFpNyh7mRnrhVVUQx michael@dodwell.us"
-    ];
-  };
 
   # SSH configuration
   services.openssh = {
--- system/core/users.nix
+++ system/core/users.nix
@@ -10,6 +10,10 @@
       "video"
       "wheel"
       "kvm"
+      "docker"
+    ];
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAII1Vk18qExSQM6rksG500xD/mgACFpNyh7mRnrhVVUQx michael@dodwell.us"
     ];
   };
 }
